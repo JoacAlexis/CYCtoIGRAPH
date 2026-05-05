11bafce0a4c37b10228a842ffa114d8084954076
JoacAlexis <Joachim.Alexis.Ibsen@gmail.com>
2026-05-05 14:47:53 +0200

    new script

--- cyc_convert.py
+++ cyc_convert.py
@@ -1,3 +1,11 @@
 # Program for converting BioCyc databse to R igraph format
 
 #BioCyc data file format
+# 5.0
+# |
+# |---- data
+# |       |---- gene.dat   # Genes to internal IDs
+# |       |---- protein.dat # intenal IDs+ Gene IDs to reaction
+# |---- input
+# |---- kb
+# |---- reports
